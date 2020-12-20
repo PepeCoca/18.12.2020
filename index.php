837130a57cc519ae8c746fe9b177aead46c26f4d
Nikolay Malyshev <kop.kot.14@mail.ru>
2020-12-20 12:22:35 +0500

    18.12.2020_2.ZD

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
-<?
+<?/*
 
 echo "Малышев",PHP_EOL;
 echo "Николай ",PHP_EOL;
 echo "Сергеевич ".'<br/>';
 echo "Возраст:20 лет ";
+*/?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style03.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
+<p> <?php echo "Малышев",PHP_EOL;
+    echo "Николай ",PHP_EOL;
+    echo "Сергеевич ".'<br/>';
+    echo "Возраст:20 лет "
+    ?> </p>
+
+</body>
+</html>
 
